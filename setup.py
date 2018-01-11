28c4e5f893d108293f877f9212c594bc9a3c7c1b
Jakub Valenta <jakub@jakubvalenta.cz>
2018-01-11 23:30:11 +0100

    setup: Add python ver classifiers

--- setup.py
+++ setup.py
@@ -31,6 +31,9 @@ setup(
         'Topic :: Software Development',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 
     keywords='',
