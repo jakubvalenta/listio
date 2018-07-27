8f55a186933755a9d0abb86e87f6381ae28e27af
Jakub Valenta <jakub@jakubvalenta.cz>
2018-07-27 20:31:58 +0200

    setup: Bump minor version to 1.1.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(
     name='listio',
 
-    version='1.0.1',
+    version='1.1.0',
 
     description='Read/write lists and maps (two dimensional lists) from/to'
     ' files.',
